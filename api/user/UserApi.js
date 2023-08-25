ad0b8b70e80c3142f1feb29e70df7d4a72763765
eyerusalem <eyerusad12@gmail.com>
2023-08-25 17:19:53 +0300

    nodemon added to devDependencies

--- api/user/UserApi.js
+++ api/user/UserApi.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userViewModel = require("./UserViewModel")
 const validation = require("../Middleware/Validation")
 const user = require("./UserHandler")
+
 router.route("/").post(userViewModel.userSignInViewModel, validation, user.postUsers);
 
 
