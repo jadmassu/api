75fd22762af3f7e79bc008d4e74c687609cd12b8
eyerusalem <eyerusas12@gmail.com>
2023-11-14 22:41:15 +0300

    user model changed

--- index.ts
+++ index.ts
@@ -1,31 +1,22 @@
 
-
-
 import express, { Application, Request, Response } from 'express';
 import { connectToDatabase } from './src/utils/sequelize';
-// import 'dotenv/config';
-// require("dotenv").config({ path: "./config.env" });
 import dotenv from 'dotenv';
 dotenv.config({ path: './config.env' });
 
-// import config from './config';
+
 
 
 const app: Application = express();
 
 
-app.use(express.json({ limit: "500mb" }));
-
-
-
+app.use(express.json({ limit: "50mb" }));
 
 app.use("/user", require("./src/user/User.api"))
 
-// app.get("/", (req, res) => {
-//     res.send("<h2>some mmessage</h2>");
-// });
+
 app.get("/", (req: Request, res: Response) => {
-    res.send("<h2>some mmessage</h2>");
+    res.send("<h2>some message</h2>");
 
 });
 
